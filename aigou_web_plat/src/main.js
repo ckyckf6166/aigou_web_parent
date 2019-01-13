9fd8f63aa33f171b05c0ce0ce25459d3156d310c
ckyckf <ckyckf@qq.com>
2019-01-13 14:37:33 +0800

    提交

--- aigou_web_plat/src/main.js
+++ aigou_web_plat/src/main.js
@@ -3,33 +3,41 @@ import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
-//import './assets/theme/theme-green/index.css'
 import VueRouter from 'vue-router'
 import store from './vuex/store'
 import Vuex from 'vuex'
 //import NProgress from 'nprogress'
 //import 'nprogress/nprogress.css'
 import routes from './routes'
-import Mock from './mock'
-Mock.bootstrap();
+//import Mock from './mock'
+//Mock.bootstrap();
 import 'font-awesome/css/font-awesome.min.css'
 
 Vue.use(ElementUI)
 Vue.use(VueRouter)
 Vue.use(Vuex)
-
 //NProgress.configure({ showSpinner: false });
+
+//对axios进行统一配置
+import axios from 'axios' //和引入vue一样
+axios.defaults.baseURL = "http://127.0.0.1:9527/services"//网关地址 配置前缀
+
+// 将API方法绑定到全局
+Vue.prototype.$http = axios //取别名  给Vue这个类添加一个原型的属性,这个类的对象都能调用
+Vue.config.productionTip = false
 
 const router = new VueRouter({
   routes
 })
-
+//路由每次执行前,都会判断是否登录
 router.beforeEach((to, from, next) => {
   //NProgress.start();
   if (to.path == '/login') {
     sessionStorage.removeItem('user');
   }
+  //从session获取用户
   let user = JSON.parse(sessionStorage.getItem('user'));
+  //null表示false !null=true
   if (!user && to.path != '/login') {
     next({ path: '/login' })
   } else {
